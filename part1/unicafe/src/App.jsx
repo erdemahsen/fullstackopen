8ecaaefe7aa37bcc83e98065bb4a2dc2953cb841
erdemahsen <omer.ahsen@metu.edu.tr>
2025-12-09 19:29:42 +0300

    ex 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,6 +4,35 @@ const Button = (props) => {
   return <button onClick={props.onClick}>{props.text}</button>
 }
 
+const Statistics = ({data}) => {
+
+  const {good, neutral, bad} = data
+  if (!(good || neutral || bad)){
+    return(
+   <>
+    <h1>statistics</h1>
+    <p>
+      No feedback given
+    </p>
+  </>
+  )
+  }
+  return(
+   <>
+    <h1>statistics</h1>
+    <p>
+      good {good} <br/>
+      neutral {neutral} <br/>
+      bad {bad} <br/>
+      all {good + neutral + bad} <br/>
+      average {(good - bad) / (good + bad + neutral)} <br/>
+      positive {(good) / (good + bad + neutral) * 100} % <br/>
+    </p>
+  </>
+  )
+}
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -16,15 +45,9 @@ const App = () => {
       <Button onClick={() => setGood(good + 1)} text ="good"/>
       <Button onClick={() => setNeutral(neutral + 1)} text ="neutral"/>
       <Button onClick={() => setBad(bad + 1)} text ="bad"/>
-      <h1>statistics</h1>
-      <p>
-        good {good} <br/>
-        neutral {neutral} <br/>
-        bad {bad} <br/>
-        all {good + neutral + bad} <br/>
-        average {(good - bad) / (good + bad + neutral)} <br/>
-        positive {(good) / (good + bad + neutral) * 100} % <br/>
-      </p>
+
+      <Statistics data={{good, neutral, bad}}/>
+      
 
     </div>
   )
