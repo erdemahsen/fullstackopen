8b4ba7e3df61cd33c1daee9076be0d78c3309c68
erdemahsen <omer.ahsen@metu.edu.tr>
2025-12-11 23:03:45 +0300

    ex 3.8*

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,8 +3,12 @@ const morgan = require('morgan');
 
 const app = express();
 
+morgan.token('dataJson', (request, response) => {
+    return JSON.stringify(request.body)
+})
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :dataJson'))
 
 let personsData = [
     { 
