2dc861e320a4f63568425b25611c4226aebcf21d
erdemahsen <omer.ahsen@metu.edu.tr>
2025-12-17 01:15:01 +0300

    ex 3.17*

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -71,6 +71,7 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
+
     if(!body.name || !body.number)
     {
         return response.status(404).json(
@@ -90,6 +91,33 @@ app.post('/api/persons', (request, response) => {
         })
         .catch(e => console.log(e))
     
+})
+
+app.put('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+
+    const body = request.body
+
+    if(!body.name || !body.number)
+    {
+        return response.status(404).json(
+            {
+                "error": "name or number is missing"
+            }
+        )
+    }
+
+    Person.findById(id)
+        .then(person => {
+            if(!person){
+                return response.status(404).end()
+            }
+            person.number = body.number
+
+            return person.save().then(updatedPerson => response.json(updatedPerson))
+        })
+
+
 })
 
 const errorHandler = (error, request, response, next) => {
