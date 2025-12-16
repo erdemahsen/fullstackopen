6a90f7ab3e1a6862f1252c198f57c5d2c58c0ea1
erdemahsen <omer.ahsen@metu.edu.tr>
2025-12-17 00:07:48 +0300

    ex 3.13-3.14

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -16,43 +16,16 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :dataJson'))
 app.use(express.static('dist'))
 
-
-//const url = process.env.MONGODB_URI
-//console.log(url)
-
-
-let personsData = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/', (request, response) => {
     response.send('health check')
 })
 
+// TODO
 app.get('/api/info', (request, response) => {
     const now = new Date()
     //const infoText = "<p>Phonebook has info for " + String(personsData.length) + " people" + "\n" + now
 
-    const infoText = `<p>Phonebook has info for ${String(personsData.length)} people <br/> ${now}</p>`
+    const infoText = `<p>Time ${now}</p>`
     
     response.send(infoText)
 })
@@ -84,12 +57,16 @@ app.get('/api/persons/:id', (request, response) => {
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
     //personsData = personsData.filter(p => p.id != id)
-    Person.deleteOne({
-        id: id
-    })
-    .then(lel => {console.log(lel)})
-    .catch(e => console.log(e))
-    response.status(204).end("Person successfully deleted")
+    Person.deleteOne({_id: id})
+        .then(res => {
+            if(res.deletedCount > 0){
+                response.status(204).end("Person successfully deleted")
+            }
+            else{
+                response.status(204).end(`Person with id ${id} does not exists`)
+            }
+        })
+        .catch(e => console.log(e))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -102,23 +79,17 @@ app.post('/api/persons', (request, response) => {
             }
         )
     }
-    else if(personsData.filter(p => p.name === body.name).length > 0){
-        return response.status(404).json(
-            {
-                "error": "name must be unique"
-            }
-            
-        )
-    }
 
-    const person = {
-        id: String(Math.floor(Math.random()*1000)),
+    const person = new Person({
         name: body.name,
         number: body.number,
-    }
-    console.log(person, "Successfully created")
-    personsData = personsData.concat(person)
-    response.json(person)
+    })
+    person.save()
+        .then(p => {
+            response.json(p)
+        })
+        .catch(e => console.log(e))
+    
 })
 
 const PORT = process.env.PORT || 3001
