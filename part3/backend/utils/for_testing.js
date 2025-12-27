777c546f59f5168bbe01ad54752df05958a77795
erdemahsen <omer.ahsen@metu.edu.tr>
2025-12-28 01:26:37 +0300

    ex 4.3

---
+++ part3/backend/utils/for_testing.js
@@ -0,0 +1,21 @@
+const reverse = (string) => {
+  return string
+    .split('')
+    .reverse()
+    .join('')
+}
+
+const average = (array) => {
+    if(array.length == 0)
+        return 0
+  const reducer = (sum, item) => {
+    return sum + item
+  }
+
+  return array.length === 0 ? 0 :  array.reduce(reducer, 0) / array.length
+}
+
+module.exports = {
+  reverse,
+  average,
+}
