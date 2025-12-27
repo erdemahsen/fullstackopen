89f16716fd9e8136dd183f7cb094dc8979c95fea
erdemahsen <omer.ahsen@metu.edu.tr>
2025-12-28 01:39:03 +0300

    ex 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,15 @@
+const blog = require("../models/blog")
+
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+    const sum = (a, b)  => a+b
+    return blogs.map(b => b.likes).reduce(sum, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
