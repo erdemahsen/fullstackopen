154c1d702f2ab7461051aa8d000f138b11eda52d
erdemahsen <omer.ahsen@metu.edu.tr>
2025-12-29 17:37:15 +0300

    ex 4.14 - without tests backend is here, I can make it look better though

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -40,5 +40,26 @@ blogsRouter.delete('/:id', async(request, response, next) => {
   }
 })
 
+blogsRouter.put('/:id', async(request, response, next) => {
+  try {
+    const blogToUpdate = await Blog.findById(request.params.id)
+    const updatedContent = request.body
+
+    //console.log(blogToUpdate)
+    //console.log(updatedContent)
+    blogToUpdate.title = updatedContent.title
+    blogToUpdate.author = updatedContent.author
+    blogToUpdate.url = updatedContent.url
+    blogToUpdate.likes = updatedContent.likes
+
+    const updateResponse = await blogToUpdate.save()
+
+    response.json(updateResponse)
+  }
+  catch(error) {
+    next(error)
+  }
+})
+
 module.exports = blogsRouter
 
