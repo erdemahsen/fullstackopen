d6dc21b87994d843942cdf07f654cdffe53fb902
erdemahsen <omer.ahsen@metu.edu.tr>
2025-12-29 15:56:08 +0300

    ex 4.8 - done

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,10 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  //console.log(blogs)
+  response.json(blogs)
+  // refactored it to use async await
+  
+  //Blog.find({}).then((blogs) => {
+  //  response.json(blogs)
+  //})
 })
 
 blogsRouter.post('/', (request, response) => {
