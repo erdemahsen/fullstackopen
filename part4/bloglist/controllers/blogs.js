d0b4c267fd2af8d7b675156a04f00dc0617d4cc7
erdemahsen <omer.ahsen@metu.edu.tr>
2025-12-29 17:02:54 +0300

    ex 4.12 - instead of directy sending the 400 error, let our middleware handle that. Feels better

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,10 +1,14 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
-  //console.log(blogs)
-  response.json(blogs)
+blogsRouter.get('/', async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({})
+    response.json(blogs)
+  }
+  catch(error) {
+    next(error)
+  }
   // refactored it to use async await
   
   //Blog.find({}).then((blogs) => {
@@ -12,11 +16,15 @@ blogsRouter.get('/', async (request, response) => {
   //})
 })
 
-blogsRouter.post('/', async (request, response) => {
-  const blogObj = new Blog(request.body)
-
-  const blog = await blogObj.save()
-  response.status(201).json(blog)
+blogsRouter.post('/', async (request, response, next) => {
+  try {
+    const blogObj = new Blog(request.body)
+    const blog = await blogObj.save()
+    response.status(201).json(blog)
+  }
+  catch(error) {
+    next(error)
+  }
 
   //blog.save().then((result) => {
   //  response.status(201).json(result)
