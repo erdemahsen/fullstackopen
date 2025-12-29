e63dfbdfc04e23b31f4caf2301b0951eee6f7d2a
erdemahsen <omer.ahsen@metu.edu.tr>
2025-12-29 15:47:48 +0300

    ex 4.8 - tests are written nicely, now I have to refactor route handlers

--- part4/bloglist/tests/blog_api_test.js
+++ part4/bloglist/tests/blog_api_test.js
@@ -1,9 +1,35 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
+const strict = require('assert/strict')
+const { strictEqual } = require('assert')
 
 const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: "Z Kuşağı Jargonu",
+    author: "Ali",
+    url: "https://www.alidindar.tr/z-kusagi-jargonu-2025/",
+    likes: 3
+  },
+  {
+    title: "Rusya Ukrayna ne olacak",
+    author: "Andreas",
+    url: "https://www.eurotopics.net/tr/350544/ukrayna-savasi-yeni-yil-ne-getirecek",
+    likes: 11
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
 
 test('blogs are returned as json', async () => {
   await api
@@ -11,6 +37,12 @@ test('blogs are returned as json', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  strictEqual(response.body.length, initialBlogs.length)
+})
+
 
 after(async () => {
   await mongoose.connection.close()
