afc5c67dc6ecf7f8b924eec2ca61bf8d58b0ce3f
erdemahsen <omer.ahsen@metu.edu.tr>
2026-01-02 17:40:24 +0300

    ex 5.19

--- part5/playwright/tests/blog_app.spec.js
+++ part5/playwright/tests/blog_app.spec.js
@@ -1,14 +1,32 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
+const blog = {
+  title: 'Blog E2E',
+  author: 'Testerr',
+  url: 'hello.com',
+  //likes: 0,
+  //user: {
+  //  username: 'mluukkai',
+  //  name: 'Matti Luukkainen',
+  //}
+}
+
+const user = {
+  username: 'mluukkai',
+  name: 'Matti Luukkainen',
+  password: 'salainen',
+}
+
+
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
 
     await request.post('http://localhost:3001/api/testing/reset')
     await request.post('http://localhost:3001/api/users', {
       data: {
-        name: 'Matti Luukkainen',
-        username: 'mluukkai',
-        password: 'salainen'
+        name: user.name,
+        username: user.username,
+        password: user.password
       }
     })
 
@@ -23,20 +41,65 @@ describe('Blog app', () => {
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-      await page.getByLabel('username').fill('mluukkai')
-      await page.getByLabel('password').fill('salainen')
+      await page.getByLabel('username').fill(user.username)
+      await page.getByLabel('password').fill(user.password)
       await page.getByRole('button', { name: 'login' }).click()
       // 
       await expect(page.getByText('Matti Luukkainen is logged in')).toBeVisible()
     })
 
     test('fails with wrong credentials', async ({ page }) => {
-      await page.getByLabel('username').fill('mluukkai')
-      await page.getByLabel('password').fill('wrong')
+      await page.getByLabel('username').fill(user.username)
+      await page.getByLabel('password').fill("WRONG PASSword")
       await page.getByRole('button', { name: 'login' }).click()
 
       await expect(page.getByText('username')).toBeVisible()
       await expect(page.getByText('wrong username or password')).toBeVisible()
     })
   })
+
+    describe('When logged in', () => {
+
+        beforeEach(async ({ page }) => {
+            await page.getByLabel('username').fill(user.username)
+            await page.getByLabel('password').fill(user.password)
+            await page.getByRole('button', { name: 'login' }).click()
+            // await expect(page.getByText('Matti Luukkainen is logged in')).toBeVisible()
+            await expect(page.getByText(`${user.name} is logged in`)).toBeVisible()
+        })
+
+        test('a new blog can be created', async ({ page }) => {
+            await page.getByRole('button', { name: 'Create new blog' }).click()
+            await page.getByLabel('title').fill(blog.title)
+            await page.getByLabel('author').fill(blog.author)
+            await page.getByLabel('url').fill(blog.url)
+            await page.getByRole('button', { name: 'addBlog' }).click()
+            // checking the notification
+            await expect(page.getByText(`New blog ${blog.title} by ${blog.author} is added`)).toBeVisible()
+            await expect(page.getByText(`${blog.title} - ${blog.author}`)).toBeVisible()
+        })
+
+        describe('After creating the blog', () => {
+            beforeEach(async ({ page }) => {
+                await page.getByRole('button', { name: 'Create new blog' }).click()
+                await page.getByLabel('title').fill(blog.title)
+                await page.getByLabel('author').fill(blog.author)
+                await page.getByLabel('url').fill(blog.url)
+                await page.getByRole('button', { name: 'addBlog' }).click()
+                // checking the notification
+                await expect(page.getByText(`New blog ${blog.title} by ${blog.author} is added`)).toBeVisible()
+                await expect(page.getByText(`${blog.title} - ${blog.author}`)).toBeVisible()
+
+                
+            })
+
+            test('a new blog can be created', async ({ page }) => {
+                await page.getByRole('button', { name: 'view' }).click()
+                await expect(page.getByText("likes: 0")).toBeVisible()
+                await page.getByRole('button', { name: 'like' }).click()
+                await expect(page.getByText("likes: 1")).toBeVisible()
+            })
+        })
+    })
+
 })
