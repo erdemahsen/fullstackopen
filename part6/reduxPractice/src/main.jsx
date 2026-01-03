764fa9561597a65dfd0dde865518f6f199eb021b
erdemahsen <omer.ahsen@metu.edu.tr>
2026-01-03 17:17:56 +0300

    before switching to the hooks

--- part6/reduxPractice/src/main.jsx
+++ part6/reduxPractice/src/main.jsx
@@ -22,12 +22,46 @@ store.dispatch({
   }
 })
 
+const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+
+const createNote = content => {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+const toggleImportanceOf = id => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: { id }
+  }
+}
+
 const App = () => {
+  const addNote = event => {
+    event.preventDefault()
+    const content = event.target.note.value // Please note that the input field must have a name in order to access its value
+    event.target.note.value = ''
+    store.dispatch(createNote(content)) // our component does not have to know about create note 
+  }
+
+  const toggleImportance = id => {
+    store.dispatch(toggleImportanceOf(id))
+  }
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name="note" /> 
+        <button type="submit">add</button>
+      </form>
       <ul>
         {store.getState().map(note => (
-          <li key={note.id}>
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
             {note.content} <strong>{note.important ? 'important' : ''}</strong>
           </li>
         ))}
