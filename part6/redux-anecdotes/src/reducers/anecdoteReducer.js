5b35c66de9345a93b5e718f616e82b6c8d12dbdf
erdemahsen <omer.ahsen@metu.edu.tr>
2026-01-05 00:37:02 +0300

    ex 6.14-6.15

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,13 +1,5 @@
 import { act } from "react"
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -19,14 +11,13 @@ const asObject = anecdote => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
 
 // Slice is here using toolkit
 import { createSlice, current } from '@reduxjs/toolkit'
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers: {
     addAnecdote(state, action) {
       const content = action.payload
@@ -43,10 +34,14 @@ const anecdoteSlice = createSlice({
       return state.map(anecdote =>
         anecdote.id !== id ? anecdote : anecdoteVoted 
       ).sort((a, b) => b.votes - a.votes) // I love this trick to sort :)
+    },
+    setAnecdotes(state, action) {
+      return action.payload
     }
   },
+
 })
 
 
-export const { addAnecdote, voteAnecdote } = anecdoteSlice.actions
+export const { addAnecdote, voteAnecdote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
