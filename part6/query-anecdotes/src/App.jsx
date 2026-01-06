0e24485cb911121df9029fb4a2cb95f8585e1183
erdemahsen <omer.ahsen@metu.edu.tr>
2026-01-06 15:35:05 +0300

    Before ex 6.20

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,61 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+import { useQuery } from '@tanstack/react-query'
+
+const App = () => {
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  //const anecdotes = [
+    //{
+      //content: 'If it hurts, do it more often',
+     // id: '47145',
+     // votes: 0,
+    //},
+  //]
+
+  const result = useQuery(
+  {
+    queryKey: ['anecdotes'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3001/anecdotes')
+      if (!response.ok) {
+        throw new Error('Failed to fetch notes')
+      }
+      return await response.json()
+    },
+    retry: false
+  }
+)
+
+  console.log(JSON.parse(JSON.stringify(result)))
+ 
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+ 
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
